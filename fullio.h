5269f37faa3918acc16ca743c336cd946015c79b
James W. McClain <jmcclain@daystrom-data-concepts.com>
2017-10-14 21:35:56 -0400

    Rename fullwrite.c To fullio.c

--- fullio.h
+++ fullio.h
@@ -1,5 +1,5 @@
-#ifndef __FULLWRITE_H__
-#define __FULLWRITE_H__
+#ifndef __FULLIO_H__
+#define __FULLIO_H__
 
 void fullwrite(int fd, const void * buffer, int bytes);
 void fullread(int fd, void * buffer, int bytes);
